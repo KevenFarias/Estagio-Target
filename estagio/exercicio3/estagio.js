3bf93ef97311ab082de17b6ef9d9fed27ddc8b8d
Keven Gabriel Farias <65475766+KevenFarias@users.noreply.github.com>
2024-11-22 15:53:01 -0300

    Agora o exercicio 3 funciona sem localhost

---
+++ estagio/exercicio3/estagio.js
@@ -0,0 +1,20 @@
+fetch("https://capable-rugelach-5b5558.netlify.app/dados.json")
+  .then(response => response.json())
+  .then(data => {
+    const faturamento = data
+
+    const valoresFaturamento = faturamento.map(item => item.valor)
+
+    const menorFaturamento = Math.min(...valoresFaturamento)
+
+    const maiorFaturamento = Math.max(...valoresFaturamento)
+
+    const media = valoresFaturamento.reduce((total, quantidade) => total + quantidade) / valoresFaturamento.length
+
+    const diasAcimaDaMedia = valoresFaturamento.filter(valor => valor > media).length
+
+    console.log(`Menor faturamento diário: R$ ${menorFaturamento}`)
+    console.log(`Maior faturamento diário: R$ ${maiorFaturamento}`)
+    console.log(`Numero de dias com faturamento acima da méria mensal: ${diasAcimaDaMedia}`)
+  })
+  .catch(error => console.log(error))
